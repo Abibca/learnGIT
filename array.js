fb90286b87e72a1cc3e8257c61b14f83824f7c7a
Abibca <abinayaanju2507@gmail.com>
2023-06-02 11:29:46 +0530

    first

--- array.js
+++ array.js
@@ -2,8 +2,14 @@ let users = [
     {firstname:'indra',lastname:'sena',age:20},
     { firstname: 'Destratum', lastname: 'solutions', age: 19 },
     { firstname: 'taminadu', lastname: 'chennai', age: 18 }
+    //"bfhnfbbvhnfbfbf"
 ]
 
 
 
 console.log(users.filter(x => x.age<20).map((x) => x.firstname+" "+x.lastname));
+const ages = [32,33,16,40];
+const result = ages.filter(checkadult);
+function checkadult(age){
+    return age >= 18;
+}
